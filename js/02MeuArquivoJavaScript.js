aba51c583f9a92027a7c3ff1c99ae29874e3a22c
Lian <lian.h.silva@aluno.senai.br>
2025-02-19 21:45:46 -0300

    update

--- js/02MeuArquivoJavaScript.js
+++ js/02MeuArquivoJavaScript.js
@@ -1,6 +1,11 @@
 window.onload = function(){
     let p = document.getElementById("id_p");
     console.log(p);
-    p = document.getElementById("id_p");
     p.style.backgroundColor = "#aaa";
+    let links = p.getElementsByTagName("");
+    alert(links[0]);
+}
+
+function myFunc(){
+    console.log();
 }
